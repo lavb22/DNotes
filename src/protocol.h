5c3be173e7090a87ecd558153f1e9b81a3ea4e21
DNotesCoin <contact@dnotescoin.com>
2017-08-14 20:02:18 -0400

    Revert "Fixing port selection"

--- src/protocol.h
+++ src/protocol.h
@@ -20,7 +20,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 19117 : 11224;
+    return testnet ? 19117 : 11223;
 }
 
 
